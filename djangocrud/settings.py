0525c8ea42c38aa44526513190b3e33993536c3c
OMiller555 <analyticshydra@gmail.com>
2026-04-21 10:18:21 -0500

    database(fix): liberar restriccion de logros multiples (fake local)

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -160,7 +160,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': config('DB_NAME', default='learning_labs_db'),
             'USER': config('DB_USER', default='postgres'),
-            'PASSWORD': config('DB_PASSWORD', default=''),
+            'PASSWORD': '9967112fhr',
             'HOST': config('DB_HOST', default='localhost'),
             'PORT': config('DB_PORT', default='5432'),
         }
